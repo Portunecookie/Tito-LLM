4d913497ca0f7dc0da873c6e8a36d81d8431aa83
Poxios <poxios0310@gmail.com>
2024-08-28 15:04:54 +0900

    fixed

--- server.py
+++ server.py
@@ -45,6 +45,7 @@ if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8977)
 
     # Kuma Health Check
+    print("Starting Kuma Health Check")
     while True:
         try:
             requests.get(
